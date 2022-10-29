7ec9124d78c7d82f8b664625a6572bf2c3d14eaf
Engincan Göçer <98960286+Enginar220@users.noreply.github.com>
2022-10-29 15:03:50 +0300

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -1,15 +1,17 @@
-#Here is the number that wanted to be guess correctly by user 
+from random import randint 
 
-from random import randint
-
-determined_number=randint(1,100)
+determined_number=randint(1,100) # Number that is guessed by computer
 
 while True :
-    user_guess=int(input("Take a guess between 1-100:"))
+    
+    user_guess = int(input("Take a guess between 1-100:"))
+    
     if user_guess == determined_number :
         print("you did guess correctly the number.")
         break
+        
     elif user_guess > determined_number :
         print("determined number is lesser than what you did choose.")
+        
     elif  user_guess < determined_number :
         print("determined number is greater than what you did choose .")
